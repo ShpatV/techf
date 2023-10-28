eb407c4d3f20f54ef62cbef34802fc8fafeb2df7
Shpat Vata <135318451+Shpatvv@users.noreply.github.com>
2023-10-28 22:02:20 +0200

    IServiceOrder

---
+++ techFrame/src/main/java/com/example/demo/service/IOrderService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.models.InvoiceItem;
+import com.example.demo.models.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order createOrder(List<InvoiceItem> items);
+
+    List<ViewOrderDto> list();
+
+    Order getOrderDetails(String id);
+
+    void deleteById(String id);
+}
