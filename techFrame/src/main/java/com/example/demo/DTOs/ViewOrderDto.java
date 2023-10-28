8e7716a4cf5b9adb7837d606b6e47c33a25b729f
Shpat Vata <135318451+Shpatvv@users.noreply.github.com>
2023-10-28 22:22:35 +0200

    OrderService

--- techFrame/src/main/java/com/example/demo/DTOs/ViewOrderDto.java
+++ techFrame/src/main/java/com/example/demo/DTOs/ViewOrderDto.java
@@ -12,4 +12,52 @@ public class ViewOrderDto {
     private BigDecimal vat;
     private int numberOfInvoices;
 
+    public ViewOrderDto(String id, BigDecimal subTotal, BigDecimal totalAmount, BigDecimal vat, int numberOfInvoices) {
+        this.id = id;
+        this.subTotal = subTotal;
+        this.totalAmount = totalAmount;
+        this.vat = vat;
+        this.numberOfInvoices = numberOfInvoices;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(BigDecimal subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public BigDecimal getVat() {
+        return vat;
+    }
+
+    public void setVat(BigDecimal vat) {
+        this.vat = vat;
+    }
+
+    public int getNumberOfInvoices() {
+        return numberOfInvoices;
+    }
+
+    public void setNumberOfInvoices(int numberOfInvoices) {
+        this.numberOfInvoices = numberOfInvoices;
+    }
 }
+
